cd32a1c879237a353d73f2810b52fca5c443ef52
Jack2ee-dev <jack2ee.dev@gmail.com>
2021-03-10 13:50:39 +0900

    feat: implement basic ConsoleInput

--- src/main/java/racingcar/console/input/ConsoleInput.java
+++ src/main/java/racingcar/console/input/ConsoleInput.java
@@ -1,5 +1,19 @@
 package racingcar.console.input;
 
-public class ConsoleInput {
+import java.util.Scanner;
 
+public class ConsoleInput {
+    private final Scanner scanner;
+
+    public ConsoleInput(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String getCarNamesInput() {
+        return this.scanner.nextLine();
+    }
+
+    public String getTrialInput() {
+        return this.scanner.nextLine();
+    }
 }
