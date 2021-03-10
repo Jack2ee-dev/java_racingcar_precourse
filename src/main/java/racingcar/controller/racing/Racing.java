736d40dce7e3293666384578cd3522bc673747a9
Jack2ee-dev <jack2ee.dev@gmail.com>
2021-03-10 14:34:59 +0900

    feat: implement progress rule to Rule class

--- src/main/java/racingcar/controller/racing/Racing.java
+++ src/main/java/racingcar/controller/racing/Racing.java
@@ -1,5 +1,4 @@
 package racingcar.controller.racing;
 
 public class Racing {
-
 }
--- src/main/java/racingcar/domain/rule/Rule.java
+++ src/main/java/racingcar/domain/rule/Rule.java
@@ -3,6 +3,8 @@ package racingcar.domain.rule;
 import java.util.HashMap;
 import java.util.Map;
 
+import utils.RandomUtils;
+
 public class Rule {
 
     private static final Map<String, String> CAR_NAME_DELIMITER = new HashMap<String, String>() {{
@@ -13,6 +15,10 @@ public class Rule {
     private static final String DELIMITER_KOR = "DELIMITER_KOR";
     private static final String PROGRESS_BAR = "-";
     private static final String WINNER_CONCAT_DELIMITER = ", ";
+    private static final int START_INCLUSIVE = 0;
+    private static final int END_INCLUSIVE = 9;
+    private static final int LEAST_PROGRESS_CONDITION_NUMBER = 4;
+
 
     private Rule() {
     }
@@ -39,5 +45,10 @@ public class Rule {
 
     public static String getWinnerConcatDelimiter() {
         return WINNER_CONCAT_DELIMITER;
+    }
+
+    public static boolean shouldProgress() {
+        return RandomUtils.nextInt(START_INCLUSIVE, END_INCLUSIVE)
+            >= LEAST_PROGRESS_CONDITION_NUMBER;
     }
 }
